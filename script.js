e6672b3d0f8c91c23556bb8099c30c925185575d
Iuliia <iuliiaobr.io@gmail.com>
2024-04-19 18:40:44 +0700

    displayed math operations result and sliced digits

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test");
+const num = 266219;
+
+const numArray = num.toString().split("");
+
+const multipliedNumResult = numArray.reduce((a, b) => a * Number(b));
+
+const powerResult = multipliedNumResult ** 3;
+
+const twoFirstSliceDigits = powerResult.toString().substring(0, 2);
+
+console.log(`Произведение цифр числа ${num}: ${multipliedNumResult}`);
+
+console.log(
+  `Первые две цифры числа ${powerResult}: ${Number(twoFirstSliceDigits)}`
+);
