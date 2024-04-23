7b56b500346dea25cb1a385362dd431ea5fae1de
Iuliia <iuliiaobr.io@gmail.com>
2024-04-23 19:32:39 +0700

    displayed message

--- script.js
+++ script.js
@@ -1,71 +1,21 @@
-//Lesson03
+//Lesson04
+const message1 = " This is a short message";
+const message2 = "This is a long message where length is more than 30";
 
-//task 1
-let lang = "ru";
+const displayMessage = function (str) {
+  if (typeof str !== "string") {
+    alert(`The type of ${str} is not a string`);
+    return;
+  }
 
-const daysOfWeekRu = "Пон, Вт, Ср, Чт, Пт, Сб, Вс";
-const daysOfWeekEn = "Mon, Tue, Wed, Th, Fr, Sat, Sun";
-const errorMessage = "Дни недели на данном языке отсутсвуют";
+  const trimmedString = str.trim();
 
-// if statement
-if (lang === "ru") {
-  console.log(daysOfWeekRu);
-} else if (lang === "en") {
-  console.log(daysOfWeekEn);
-} else {
-  console.log(errorMessage);
-}
+  if (trimmedString.length > 30) {
+    return `${trimmedString.slice(0, 30)}...`;
+  }
 
-//switch statement
-switch (lang) {
-  case "en":
-    console.log(daysOfWeekEn);
-    break;
-  case "ru":
-    console.log(daysOfWeekRu);
-    break;
-  default:
-    console.log(errorMessage);
-}
+  return trimmedString;
+};
 
-//многомерный массив
-const daysOfWeek = [
-  [
-    "ru",
-    [
-      "Понедельник",
-      "Вторник",
-      "Среда",
-      "Четверг",
-      "Пятница",
-      "Суббота",
-      "Воскресенье",
-    ],
-  ],
-  [
-    "en",
-    [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ],
-  ],
-];
-
-const selectedLanguage = daysOfWeek.find((item) => item[0] === lang);
-console.log(selectedLanguage[1]);
-
-//task 2
-const namePerson = "Юля";
-
-console.log(
-  namePerson === "Артем"
-    ? "директор"
-    : namePerson === "Александр"
-    ? "преподаватель"
-    : "студент"
-);
+console.log(displayMessage(message1));
+console.log(displayMessage(message2));
